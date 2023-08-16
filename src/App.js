b9a12d450ece783af915cfce9bdeac2c3ab522e8
Myroslav Baibarza <tutuk6835@gmail.com>
2023-08-16 12:57:20 +0300

    Start-version1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Routing from "./routes/Routing";
+
+function App() {
+  return (
+    <div className="Add">
+      <React.StrictMode>
+        <Routing/>
+      </React.StrictMode>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/Routing.js
@@ -0,0 +1,16 @@
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
+const Routing = () => (
+  {/* 
+      <Router>
+        <Routes>
+          <Route path="/" element={<PostsList />} />
+          <Route path="/todolist" element={<TodoList />} />
+          <Route path="/userlist" element={<UserList />} />
+        </Routes>
+      </Router>
+      */}
+);
+
+export default Routing;
